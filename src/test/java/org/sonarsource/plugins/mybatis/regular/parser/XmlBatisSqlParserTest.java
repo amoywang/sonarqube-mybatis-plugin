6f8bd99e4f470ce3ddd51d34d7a75698935dd2fa
wangjianfu <wangjianfu@cndinfo.com>
2024-01-29 14:49:01 +0800

    [opt] fix unit test

--- src/test/java/org/sonarsource/plugins/mybatis/regular/parser/XmlBatisSqlParserTest.java
+++ src/test/java/org/sonarsource/plugins/mybatis/regular/parser/XmlBatisSqlParserTest.java
@@ -1,11 +1,12 @@
 package org.sonarsource.plugins.mybatis.regular.parser;
 
+import org.junit.Assert;
 import org.junit.Test;
-import org.sonarsource.plugins.mybatis.rules.ErrorDataFromLinter;
 import org.sonarsource.plugins.mybatis.regular.enums.RuleCodeEnum;
 import org.sonarsource.plugins.mybatis.regular.pojo.XmlPluginRuleResult;
 import org.sonarsource.plugins.mybatis.regular.pojo.XmlPluginRuleResultAll;
 import org.sonarsource.plugins.mybatis.regular.util.FileUtil;
+import org.sonarsource.plugins.mybatis.sql.pojo.RuleCheckResult;
 import org.sonarsource.plugins.mybatis.xml.XmlBatisSqlParser;
 import org.sonarsource.plugins.mybatis.xml.XmlParseResult;
 
@@ -17,38 +18,27 @@ import java.util.Map;
 public class XmlBatisSqlParserTest {
     @Test
     public void execute() {
-        try {
-            XmlBatisSqlParser xmlBatisSqlParser = new XmlBatisSqlParser();
-            List<String> xmlFileList = new ArrayList<>();
-            URL data = XmlBatisSqlParserTest.class.getClassLoader().getResource("mapper");
-            FileUtil.getFileList(data.getPath(), ".xml", xmlFileList);
-            List<XmlParseResult> results = xmlBatisSqlParser.parseXml(xmlFileList, "mysql");
-            System.out.printf("SQL表达式解析完成,请查看:  \n");
-            for(XmlParseResult result:results){
-                List temp = result.getXmlNodeParserResult().getRuleCheckResults();
-                if (!temp.isEmpty()){
-                    temp.forEach(item->{
-//                        System.out.println(item.toString());
-                    });
+        XmlBatisSqlParser xmlBatisSqlParser = new XmlBatisSqlParser();
+        List<String> xmlFileList = new ArrayList<>();
+        URL data = XmlBatisSqlParserTest.class.getClassLoader().getResource("mapper");
+        FileUtil.getFileList(data.getPath(), ".xml", xmlFileList);
+        List<XmlParseResult> results = xmlBatisSqlParser.parseXml(xmlFileList, "mysql");
+        Assert.assertNotNull(results);
+        Assert.assertEquals(7, results.size());
+        for (XmlParseResult xmlParseResult : results) {
+            if (xmlParseResult.getXmlNodeParserResult().getRuleCheckResults().size() > 0) {
+                List<RuleCheckResult> sqlExpressCheckResults = xmlParseResult.getXmlNodeParserResult().getRuleCheckResults();
+                for (RuleCheckResult temp : sqlExpressCheckResults) {
+                    Assert.assertTrue(temp.getRuleId().startsWith("R000"));
                 }
             }
-            XmlPluginRuleResultAll all = RegularRuleHandler.doRuleAll(results);
-            Map<RuleCodeEnum, List<XmlPluginRuleResult>> map = all.getRuleMap();
-            System.out.printf("正则表达式解析完成:\n");
-            if (map != null && map.size() > 0) {
-                for (Map.Entry<RuleCodeEnum, List<XmlPluginRuleResult>> entry : map.entrySet()) {
-                    RuleCodeEnum ruleInfo = entry.getKey();
-                    for (XmlPluginRuleResult result : entry.getValue()) {
-                        ErrorDataFromLinter errorData = new ErrorDataFromLinter(ruleInfo.getName(), ruleInfo.getDesc(),
-                                result.getFilePath(), result.getLineNumber());
-                        System.out.println(errorData);
-                    }
-                }
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
         }
-        System.exit(0);
-
+        // regular check
+        XmlPluginRuleResultAll all = RegularRuleHandler.doRuleAll(results);
+        Map<RuleCodeEnum, List<XmlPluginRuleResult>> map = all.getRuleMap();
+        Assert.assertNotNull(map);
+        Assert.assertEquals(2, map.size());
+        Assert.assertNotNull(map.get(RuleCodeEnum.R1003));
+        Assert.assertNotNull(map.get(RuleCodeEnum.R0003));
     }
 }
